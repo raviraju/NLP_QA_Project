76da726500fb5453471c14ff1888f1945e0a2c39
Thamme Gowda <thammegowda@apache.org>
2016-11-23 12:10:28 -0800

    Refactored babiLemma as babiparser

--- NERs_and_Graph/babiLemma/Globals.py
+++
@@ -1,8 +0,0 @@
-class GlobalsClass:
-
-    ENCODING="utf-8"
-    JSON_FILE="Tests/outputResultsLemma/NER_JSON_1_Supp.json"
-    NERTEXT_FILE="Tests/outputResultsLemma/NER_TEXT_1_Supp.jl"
-    LEMMA_TEXT="Tests/babiLemmaInputFile/"
-    #LEMMA_TEXT="/home/aditya/newJavaSpace/babI/Tests/Test_files/"
-    CORENLP_SERVER="http://localhost:9000"
